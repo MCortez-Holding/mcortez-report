afd6675a1176ee86956ef85a9be39d981d2d5a4f
Jorge Venegas <mariano.venegas.francia@gmail.com>
2025-09-10 10:06:34 -0500

    nuevo modulo de reporte konectar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ReporteComponent } from './reporte/reporte.component';
 import { AsesorComponent } from './asesor/asesor.component';
 import { AsesorDosComponent } from './asesor-dos/asesor-dos.component';
 import { ReportTelecomComponent } from './report-telecom/report-telecom.component';
+import { ReportKonectarComponent } from './report-konectar/report-konectar.component';
 
 const routes: Routes = [
   { path: 'reporte-general', component: ReporteComponent, pathMatch: 'full' },
   { path: 'reporte-asesor', component: AsesorComponent},
   { path: 'reporte-asesor-dos', component: AsesorDosComponent},
-  { path: 'reporte-telecom', component: ReportTelecomComponent}
+  { path: 'reporte-telecom', component: ReportTelecomComponent},
+  { path: 'reporte-konectar', component: ReportKonectarComponent}
 ];
 
 @NgModule({
